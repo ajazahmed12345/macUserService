28358f569a80c6c7c22e38b67d4079ba9c31f235
Ajaz Ahmed <ahmed786ajaz@Ajazs-MacBook-Air.local>
2023-10-22 00:22:41 +0530

    AuthController addition

---
+++ src/main/java/com/ajaz/userservice/controllers/AuthController.java
@@ -0,0 +1,18 @@
+package com.ajaz.userservice.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @PostMapping("/signup")
+    public String signup(){
+        return "signed in successfully.";
+    }
+
+
+
+}
